e956082ab173f78744a0ba820ac3b6c50b5b0a9d
Jeff Ananias <155593494+netrin-1@users.noreply.github.com>
2024-08-25 14:37:20 -0700

    Solve prompt 6 (range)

--- src/recursion.js
+++ src/recursion.js
@@ -79,6 +79,15 @@ let sumBelow = function (n) {
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 let range = function (x, y) {
+  if (x === y || x === (y + 1) || x === (y - 1)) {
+    return [];
+  }
+  if (x < y) {
+    return [x + 1].concat(range(x + 1, y));
+  }
+  if (x > y) {
+    return [x - 1].concat(range(x - 1, y));
+  }
 };
 
 // 7. Compute the exponent of a number.
